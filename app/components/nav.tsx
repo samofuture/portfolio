9067aaee0343771e1fbdc0f3151bb78d6e8130ce
Sam Crane <samovarfuture@gmail.com>
2024-11-14 19:07:01 -0500

    Removing Blog from template

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -2,19 +2,16 @@ import Link from 'next/link'
 
 const navItems = {
   '/': {
-    name: 'home',
+    name: 'Home',
   },
-  '/blog': {
-    name: 'blog',
-  },
-  'https://vercel.com/templates/next.js/portfolio-starter-kit': {
-    name: 'deploy',
-  },
+  '/projects': {
+    name: 'Projects'
+  }
 }
 
 export function Navbar() {
   return (
-    <aside className="-ml-[8px] mb-16 tracking-tight">
+    <aside className="-ml-[8px] mb-10 tracking-tight">
       <div className="lg:sticky lg:top-20">
         <nav
           className="flex flex-row items-start relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative"
