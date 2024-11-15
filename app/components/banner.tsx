9f481a7ff7f7f43756565028c5d14f06fabc86e8
Sam Crane <samovarfuture@gmail.com>
2024-11-14 19:41:14 -0500

    Changing to orange-300

--- app/components/banner.tsx
+++ app/components/banner.tsx
@@ -9,7 +9,7 @@ export default function Banner() {
         </div>
         <div className="flex flex-col items-center md:items-start">
             <h1 className="text-5xl font-bold text-white">Sam Crane</h1>
-            <h3 className="text-2xl font-semibold bg-clip-text text-transparent bg-gradient-to-r from-blue-300 to-orange-400 tracking-wide">
+            <h3 className="text-2xl font-semibold bg-clip-text text-transparent bg-gradient-to-r from-blue-300 to-orange-300 tracking-wide">
                 Software Engineer
             </h3>
             <p className="text-base tracking-wide text-center md:text-left">
